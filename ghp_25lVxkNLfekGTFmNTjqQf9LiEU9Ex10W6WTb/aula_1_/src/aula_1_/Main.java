498bdb51e2dfd58a1a9ae8975d09856d714f7126
LAB02-PC15 <LAB02-PC15@DESKTOP-L7LEES6>
2024-02-05 21:31:29 -0300

    aula 1

--- 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/aula_1_/src/aula_1_/Main.java
+++ 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/aula_1_/src/aula_1_/Main.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-	Cachorro cachorro_1 = new cachorro();	
+	Cachorro cachorro_1 = new Cachorro();	
 	cachorro_1.nome = "Rogerio";
 	cachorro_1.raca = "viralata";
 	cachorro_1.idade = 8;
 	cachorro_1.altura = 1.7;
 	cachorro_1.latir();
+	
 	cachorro_1.GetCachorro();
 	}
 			
